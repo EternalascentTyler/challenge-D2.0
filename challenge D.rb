459e3e8cb59aab047595b975f691a07bd0acfb79
EternalascentTyler <Tyler_scott@student.davincischools.org>
2025-02-18 11:34:59 -0800

    Update challenge D.rb

--- challenge D.rb
+++ challenge D.rb
@@ -1,52 +1,116 @@
-use_bpm 100
+use_bpm 136
+use_synth :piano
 
-
-define :ukulele_chords do
-  use_synth :pluck
-  play_chord chord(:c, :major), release: 0.8, amp: 1.5
-  sleep 0.5
-  play_chord chord(:g, :major), release: 0.8, amp: 1.5
-  sleep 0.5
-  play_chord chord(:f, :major), release: 0.8, amp: 1.5
-  sleep 0.5
-  play_chord chord(:c, :major), release: 0.8, amp: 1.5
+#DON'T CHANGE THIS LIVE LOOP
+live_loop:background_notes do
+  play:E3, amp: 0.25
   sleep 1
-end
-
-
-define :spongebob_melody do
-  use_synth :fm
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
   
-  play :e5, slide: 0.2, release: 0.5
-  sleep 0.5
-  play :d5, slide: 0.2, release: 0.5
-  sleep 0.5
-  play :c5, slide: 0.2, release: 0.5
-  sleep 0.5
-  play :b4, slide: 0.2, release: 0.5
-  sleep 0.5
-  play :a4, release: 1
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+#optomize this with functions
+# Measure 1
+play :r
+sleep 1
+play:E4
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 1
+
+# Measure 2
+define :measure2 do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
   sleep 1
 end
 
-
-live_loop :drums do
-  sample :drum_cymbal_closed, amp: 0.5
-  sleep 0.5
-  sample :drum_snare_soft, amp: 0.5
-  sleep 0.5
+# Measure 3
+define :measure3 do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+#Measure 4
+measure3
+# Measure 5
+define :measure5 do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
 end
 
-live_loop :ocean do
-  sample :ambi_haunted_hum, rate: 0.3, amp: 0.3
-  sleep 4
-end
-
-
-live_loop :sadoutro do
-  ukulele_chords
-  spongebob_melody
-  sleep 2
-end
+#Measure 6
+measure3
+# Measure 7
+measure5
+# Measure 8
+measure2
+#ending
+sleep 10.7
+sample :drum_cowbell, amp:9
 
 #i tried my best
